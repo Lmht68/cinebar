83afdce0a7182c83c6419149873b24bc188bc795
ThinhLe881 <thinhleminh201@gmail.com>
2026-01-03 01:18:55 -0500

    Change app icon and display name

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -6,7 +6,7 @@
 MainWindow::MainWindow()
 {
     setWindowTitle(WINDOW_TITLE);
-    setWindowIcon(QIcon(":/app-icon.png"));
+    setWindowIcon(QIcon(":/icons/app-icon.png"));
     resize(1280, 720);
     // Center the window on the primary screen
     move(QGuiApplication::primaryScreen()->availableGeometry().center() - this->rect().center());
