c33d4e15c062b1b7cdcf91288fbf3bbd694994f0
ThinhLe881 <thinhleminh201@gmail.com>
2026-02-27 11:09:59 -0500

    Add project files.

---
+++ cinebar-generator.cpp
@@ -0,0 +1,12 @@
+﻿// cinebar-generator.cpp : Defines the entry point for the application.
+//
+
+#include "cinebar-generator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
